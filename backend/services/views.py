7bfdd2878a640c2f31fdfc58ee1856d22b79533a
Alexander Malshakov <89681115653@mail.ru>
2024-02-16 14:42:07 +0300

    Оптимизирует SubscriptionView

--- backend/services/views.py
+++ backend/services/views.py
@@ -2,17 +2,18 @@ from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Subscription
-from .serializers import SubscriptionSerializers
+from .serializers import SubscriptionSerializer
 from clients.models import Client
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        "plan",
         Prefetch(
             "client",
             queryset=Client.objects.all()
             .select_related("user")
             .only("company_name", "user__email"),
-        )
+        ),
     )
-    serializer_class = SubscriptionSerializers
+    serializer_class = SubscriptionSerializer
