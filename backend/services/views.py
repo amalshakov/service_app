80ef047b2f99e3999cecc800cf79cae30aecd3af
Alexander Malshakov <89681115653@mail.ru>
2024-02-18 14:39:34 +0300

    Добавляет .annotate в SubscriptionView

--- backend/services/views.py
+++ backend/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Subscription
@@ -7,13 +7,20 @@ from clients.models import Client
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related(
-        "plan",
-        Prefetch(
-            "client",
-            queryset=Client.objects.all()
-            .select_related("user")
-            .only("company_name", "user__email"),
-        ),
+    queryset = (
+        Subscription.objects.all()
+        .prefetch_related(
+            "plan",
+            Prefetch(
+                "client",
+                queryset=Client.objects.all()
+                .select_related("user")
+                .only("company_name", "user__email"),
+            ),
+        )
+        .annotate(
+            price=F("service__full_price")
+            - F("service__full_price") * F("plan__discount_percent") / 100
+        )
     )
     serializer_class = SubscriptionSerializer
