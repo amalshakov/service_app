13e97d99d4d38ed365fd3056eb95ae99dd4eba06
Alexander Malshakov <89681115653@mail.ru>
2024-03-05 13:29:07 +0300

    Change Task annotate

--- backend/services/views.py
+++ backend/services/views.py
@@ -7,20 +7,14 @@ from .serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = (
-        Subscription.objects.all().prefetch_related(
-            "plan",
-            Prefetch(
-                "client",
-                queryset=Client.objects.all()
-                .select_related("user")
-                .only("company_name", "user__email"),
-            ),
-        )
-        # .annotate(
-        #     price=F("service__full_price")
-        #     - F("service__full_price") * F("plan__discount_percent") / 100
-        # )
+    queryset = Subscription.objects.all().prefetch_related(
+        "plan",
+        Prefetch(
+            "client",
+            queryset=Client.objects.all()
+            .select_related("user")
+            .only("company_name", "user__email"),
+        ),
     )
     serializer_class = SubscriptionSerializer
 
