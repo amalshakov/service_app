9b3c6e0953cdcec51e0502aca662effce82c7b84
Alexander Malshakov <89681115653@mail.ru>
2024-03-08 11:54:53 +0300

    Add django-cachalot

--- backend/services/models.py
+++ backend/services/models.py
@@ -79,8 +79,8 @@ class Subscription(models.Model):
     def save(self, *args, **kwargs) -> None:
         creating = not bool(self.id)
         result = super().save(*args, **kwargs)
-        # if creating:
-        #     set_price.delay(self.id)
+        if creating:
+            set_price.delay(self.id)
         return result
 
 
